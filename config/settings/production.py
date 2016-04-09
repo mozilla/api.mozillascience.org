9d2d2a28fd550b29e4f78230b9e8606250935b87
Gideon Thomas <gideon@mozillafoundation.org>
2016-04-09 14:11:55 -0400

    Restart: Create Django project scienceapi

--- config/settings/production.py
+++
@@ -1,4 +0,0 @@
-from .common import *
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-DATABASES['default'] = env.db("DATABASE_URL")
--- scienceapi/urls.py
+++ scienceapi/urls.py
@@ -1,4 +1,4 @@
-"""mozillascience URL Configuration
+"""scienceapi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'', include('rest_framework_docs.urls')),
     url(r'^admin/', admin.site.urls),
 ]
