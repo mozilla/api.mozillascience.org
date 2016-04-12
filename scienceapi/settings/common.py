14ed562d9bcc03ee1e6bd5b5d535164e68aed312
alicoding <ali@alicoding.com>
2016-04-12 16:32:13 -0400

    Dockerize the scienceapi app

--- scienceapi/settings/common.py
+++ scienceapi/settings/common.py
@@ -47,9 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_docs',
-    'scienceapi.users',
-    'scienceapi.projects',
-    'scienceapi.events',
+    'users',
+    'projects',
+    'events',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -64,7 +64,7 @@ MIDDLEWARE_CLASSES = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-ROOT_URLCONF = 'scienceapi.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -82,16 +82,23 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'scienceapi.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db("DATABASE_URL", default="postgres:///mozillascience"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': '5432',
+    }
 }
-DATABASES['default']['ATOMIC_REQUESTS'] = True
+
 
 
 # Password validation
