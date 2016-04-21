69be858bacd769902c71420f2f42495900fd5700
Gideon Thomas <gideon@mozillafoundation.org>
2016-04-21 18:21:27 -0400

    Fix mozilla/science.mozilla.org#202 - Allow search functionality for getting all projects

--- app/scienceapi/projects/views.py
+++ app/scienceapi/projects/views.py
@@ -5,6 +5,16 @@ from scienceapi.projects.models import Project
 from scienceapi.projects.serializers import ProjectWithDetailsSerializer
 
 
+class ProjectSearchFilter(filters.SearchFilter):
+    """
+    We use a custom search filter to search based on a query
+    that can contain multiple terms which must be comma separated
+    """
+    def get_search_terms(self, request):
+        params = request.query_params.get(self.search_param, '')
+        return params.split(',')
+
+
 class ProjectsListView(ListAPIView):
     """
     A view that permits a GET to allow listing all the projects
@@ -13,8 +23,17 @@ class ProjectsListView(ListAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectWithDetailsSerializer
     pagination_class = None
-    filter_backends = (filters.OrderingFilter,)
+    filter_backends = (ProjectSearchFilter, filters.OrderingFilter,)
     ordering_fields = (
         'date_created',
         'date_updated',
+    )
+    search_fields = (
+        'name',
+        '=institution',
+        'description',
+        'short_description',
+        '=license',
+        '=tags__name',
+        '=categories__name',
     )
