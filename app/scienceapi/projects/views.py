fc920853f3bee42eb39c3eaf97b365479158d419
Gideon Thomas <gideon@mozillafoundation.org>
2016-04-21 18:21:27 -0400

    Fix mozilla/science.mozilla.org#202 - Allow search functionality for getting all projects

--- app/scienceapi/projects/views.py
+++ app/scienceapi/projects/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from rest_framework import filters
 
 from scienceapi.projects.models import Project
 from scienceapi.projects.serializers import ProjectWithDetailsSerializer
@@ -12,3 +13,13 @@ class ProjectsListView(ListAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectWithDetailsSerializer
     pagination_class = None
+    filter_backends = (filters.SearchFilter,)
+    search_fields = (
+        'name',
+        '=institution',
+        'description',
+        'short_description',
+        '=license',
+        '=tags__name',
+        '=categories__name',
+    )
