413cefa2c84d0e0857a232f98a3950e381ba553b
alicoding <ali@alicoding.com>
2016-05-03 14:58:22 -0400

    Add test for single project

--- app/scienceapi/projects/tests/test_views.py
+++ app/scienceapi/projects/tests/test_views.py
@@ -14,25 +14,47 @@ from scienceapi.projects.tests.test_models import (
 
 class TestProjectListView(TestCase):
 
-    def test_list_projects_returns_project_data(self):
-        user = UserFactory()
-        user.save()
-        project = ProjectFactory()
-        project.save()
-        link = ResourceLinkFactory.build(project=project)
-        link.save()
-        user_project = UserProjectFactory.build(
+    def create_user_project(self, user, project):
+        UserProjectFactory.build(
             project=project,
             user=user,
-        )
-        user_project.save()
+        ).save()
 
+    def create_link(self, project):
+        link = ResourceLinkFactory.build(project=project)
+        link.save()
+
+    def create_projects(self):
+        projects = [ProjectFactory() for i in range(3)]
+        for project in projects:
+            project.save()
+            self.create_link(project)
+        return projects
+
+    def create_user(self):
+        user = UserFactory()
+        user.save()
+        return user
+
+    def setUp(self):
+        self.projects = self.create_projects()
+        self.user = self.create_user()
+        for project in self.projects:
+            self.create_user_project(user=self.user, project=project)
+
+    def test_list_projects_returns_project_data(self):
         response = self.client.get(reverse('project-list'))
 
         self.assertEqual(response.status_code, 200)
         projects_data = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(len(projects_data), len(self.projects))
+        for project in self.projects:
+            project_serializer = ProjectSerializer(project, context={
+                'request': response.wsgi_request
+            })
+            self.assertIn(project_serializer.data, projects_data)
 
-        project_serializer = ProjectSerializer(project, context={
-            'request': response.wsgi_request
-        })
-        self.assertIn(project_serializer.data, projects_data)
+    def test_get_single_project_data(self):
+        id = self.projects[0].id
+        response = self.client.get(reverse('project', kwargs={'pk': id}))
+        self.assertEqual(response.status_code, 200)
