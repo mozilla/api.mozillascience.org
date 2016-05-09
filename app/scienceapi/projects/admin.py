925eabef31d0e707a322438bbedafc58c1cd948d
Gideon Thomas <gideon@mozillafoundation.org>
2016-05-09 15:12:29 -0400

    Allow adding links and users through the project's admin interface and projects through the tag/category admin interface

--- app/scienceapi/projects/admin.py
+++ app/scienceapi/projects/admin.py
@@ -2,7 +2,44 @@ from django.contrib import admin
 
 from .models import Project, Category, ResourceLink, Tag
 
-admin.site.register(Project)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Project.categories.through
+
+
+class TagInline(admin.TabularInline):
+    model = Project.tags.through
+
+
+class ResourceLinkInline(admin.TabularInline):
+    model = ResourceLink
+    verbose_name = 'Link'
+
+
+class UserInline(admin.TabularInline):
+    model = Project.users.through
+    verbose_name = 'User'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    inlines = [
+        TagInline,
+    ]
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [
+        ResourceLinkInline,
+        UserInline,
+    ]
+
 admin.site.register(ResourceLink)
-admin.site.register(Tag)
