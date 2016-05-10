d8491fe857dd27985b029760d8bf47f1a0efd0a2
alicoding <ali@alicoding.com>
2016-05-10 11:52:52 -0400

    Review fixes

--- app/scienceapi/study_groups/models.py
+++ app/scienceapi/study_groups/models.py
@@ -8,25 +8,18 @@ class StudyGroup(models.Model):
     name = models.CharField(max_length=300)
     link = models.URLField(
         max_length=500,
-        null=True,
-        blank=True,
         help_text='URL to the study groups\' page',
     )
     location = models.CharField(
         max_length=300,
-        null=True,
-        blank=True,
         help_text='City and or state name',
     )
     region = models.CharField(
         max_length=300,
-        null=True,
-        blank=True,
         help_text='Asia, North America, etc.'
     )
 
-    class Meta:
-        get_latest_by = 'date_created'
-
     def __str__(self):
-        return str(self.name)
+        return str('{name}, {location}, {region}'.format(
+            name=self.name, location=self.location, region=self.region
+        ))
