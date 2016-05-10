a91dfd2e56db80b0e94f86eda253320250f9c54e
alicoding <ali@alicoding.com>
2016-05-10 13:24:20 -0400

    Filter by tag and add single view
    This patch basically added a filter by tag for resource page
    and also added single view for resources and study groups API

--- app/scienceapi/resources/urls.py
+++ app/scienceapi/resources/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from scienceapi.resources.views import ResourcesListView
+from scienceapi.resources.views import ResourcesListView, ResourceView
 
 
 urlpatterns = [
     url('^$', ResourcesListView.as_view(), name='resource-list'),
+    url(r'^(?P<pk>[0-9]+)/', ResourceView.as_view(), name='resource'),
 ]
--- app/scienceapi/study_groups/views.py
+++ app/scienceapi/study_groups/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from scienceapi.study_groups.models import StudyGroup
 from scienceapi.study_groups.serializers import StudyGroupSerializer
@@ -10,6 +10,18 @@ class StudyGroupsListView(ListAPIView):
     in the database
 
     Route - `/study-groups`
+    """
+    queryset = StudyGroup.objects.all()
+    pagination_class = None
+    serializer_class = StudyGroupSerializer
+
+
+class StudyGroupView(RetrieveAPIView):
+    """
+    A view that permits a GET to allow listing of a single study group
+    by providing their `id` as a parameter
+
+    Route - `/study-groups/:id`
     """
     queryset = StudyGroup.objects.all()
     pagination_class = None
